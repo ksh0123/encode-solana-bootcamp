03bc3204d787acbce00955f562b5eb80a44a1bac
ksh0123 <shnae.k@gmail.com>
2024-04-02 22:54:29 +0900

    Rustling homework

---
+++ homeworks/homework5/variables/variables3.rs
@@ -0,0 +1,8 @@
+// variables3.rs
+// Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
+
+
+fn main() {
+    let x: i32 = 2147483647;
+    println!("Number {}", x);
+}
