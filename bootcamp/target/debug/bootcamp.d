3f53e3abd23ce16c6512a0c6aa735cefff05b22c
ksh0123 <shnae.k@gmail.com>
2024-04-02 23:12:33 +0900

    Fizz buzz program

--- bootcamp/target/debug/bootcamp.d
+++
@@ -1,1 +0,0 @@
-/Users/shinhyekang/Documents/solana-bootcamp/fizz-buzz/bootcamp/target/debug/bootcamp: /Users/shinhyekang/Documents/solana-bootcamp/fizz-buzz/bootcamp/src/main.rs
--- bootcamp/target/debug/deps/bootcamp-315367e7171e0e0d.d
+++
@@ -1,5 +0,0 @@
-/Users/shinhyekang/Documents/solana-bootcamp/fizz-buzz/bootcamp/target/debug/deps/bootcamp-315367e7171e0e0d: src/main.rs
-
-/Users/shinhyekang/Documents/solana-bootcamp/fizz-buzz/bootcamp/target/debug/deps/bootcamp-315367e7171e0e0d.d: src/main.rs
-
-src/main.rs:
