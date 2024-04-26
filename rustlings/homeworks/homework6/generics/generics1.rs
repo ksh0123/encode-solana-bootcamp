ccc10a425047f08f4c3f96fb751d770dadae4d4e
ksh0123 <shnae.k@gmail.com>
2024-04-26 23:29:43 +0900

    Rustlings Homework 6

--- rustlings/homeworks/homework6/generics/generics1.rs
+++ rustlings/homeworks/homework6/generics/generics1.rs
@@ -3,9 +3,21 @@
 
 // Execute `rustlings hint generics1` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    let mut shopping_list: Vec<String> = Vec::new();
+    shopping_list.push("milk".to_string());
+    // .to_string() creates a copy of "milk".
 }
+
+// There are two ways to use generics here:
+
+// Vec<String> | "milk".to_string()
+// Storing the `String` object
+// This method is used when you need ownership of the string
+// and the strings should be mutable.
+
+// Vec<&str> | "milk"
+// Storing the `&str` slice
+// This method is used when you know the items are static
+// and it doesn't matter if the items are read-only.
+// The string literals can be pushed to the vector.
