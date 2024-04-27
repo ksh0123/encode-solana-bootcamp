5072827aa2b0b820115d45150424a557ba2ce8cc
ksh0123 <shnae.k@gmail.com>
2024-04-27 11:09:12 +0900

    Rustlings Homework 6

--- rustlings/homeworks/homework6/generics/generics3.rs
+++ rustlings/homeworks/homework6/generics/generics3.rs
@@ -10,15 +10,20 @@
 
 // Execute 'rustlings hint generics3' for hints!
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+// Make ReportCard struct generic - grade can be a float or grade.
+// Trait Bound Syntax `<T: Trait>` - Generic T type with Display trait
+pub struct ReportCard<T: Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// Use Display Trait - automatically implements the `ToString`` trait for the type
+// `Display` is for user-facing output and cannot be derived.
+
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -46,7 +51,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
